9fcc163f7e8a5a972d18a7f5329fb01286e85558
Lucy <lucybellott@gmail.com>
2021-05-23 12:13:51 -0400

    commit tree 5f4379d23570b8ede78f19c35bd104667e4af1a2

--- index.js
+++ index.js
@@ -9,8 +9,32 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (tutorials) => {
+
+const splitArray = tutorials.split(" ");
+const capitalLetters = splitArray.map(function(){
+  return splitArray[0].toUpperCase() + splitArray.substring(1).join(" ");
+})
+  return capitalLetters
+} 
+
+
+
+
+
+
+
+
+
+
+
+/*const titleCased = tutorials => {
+  tutorials.map(function (element) {
+    const words = element.split(" ");
+    words.map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    }).join(" ");
+  })
+}*/
